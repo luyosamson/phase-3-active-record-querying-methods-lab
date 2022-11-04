cadafeb280298f3f8af4159fb30a776156be0433
luyosamson <luyosamson@gmail.com>
2022-11-04 18:15:57 +0300

    commit tree bfec86e4bc0e17718752cc71c85ca7aade81adae

---
+++ app/models/show.rb
@@ -0,0 +1,20 @@
+class Show < ActiveRecord::Base
+
+    def self.highest_rating
+
+        Show.maximum(:rating)
+
+    end
+
+    def self.most_popular_show
+        Show.where(rating=maximum)
+
+
+    end
+
+
+
+
+
+
+end
